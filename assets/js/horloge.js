6b949c400b99f59a66310bb656cf814473a5c762
WilfridDigle <wilfrid.digle@sfr.fr>
2020-10-27 15:57:46 +0100

    hebergement cv github

---
+++ assets/js/horloge.js
@@ -0,0 +1,19 @@
+window.onload=function() {
+    horloge('div_horloge');
+  };
+   
+  function horloge(el) {
+    if(typeof el=="string") { el = document.getElementById(el); }
+    function actualiser() {
+      var date = new Date();
+      var str = date.getHours();
+      str += ':'+(date.getMinutes()<10?'0':'')+date.getMinutes();
+      el.innerHTML = str;
+    }
+    actualiser();
+    setInterval(actualiser,1000);
+  }
+
+// $('#desktopExperience').dblclick(function () {
+//     $('#experience').modal('toggle');
+//   });
