fb70ec44fc918d95d07ab931806e6f7e4d74e94b
WilfridDigle <wilfrid.digle@sfr.fr>
2020-11-12 15:26:34 +0100

    ecran de demarrage complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,9 +25,6 @@ function formation() {
   function cvPapier() {
     $('#cvpapier').modal('toggle');
 }
-function justePrix() {
-  $('#justePrix').modal('toggle');
-}
 
 // Démarrage de la session
 let closeScreen = document.getElementById('closeScreen');
@@ -41,6 +38,10 @@ function disappearStartScreen() {
 }
 
 function fadeOut(){
+  var audioElement = document.getElementById('player');
+  audioElement.volume=0.10;
+  $("#player").attr('src', 'assets/wav/WindowsXPStartup.wav');
+  $("#player")[0].play();
   startScreen.classList.add("animate__animated", "animate__fadeOut");
   setTimeout(disappearStartScreen, 2000);
 }
@@ -52,59 +53,6 @@ closeScreen.onclick = function chargement(){
   }
 
 
-
-
-// Le Juste Prix !
-// var minNumber = 1; // le minimum
-// var maxNumber = 99; // le maximum
-// var randomnumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber); // la fonction magique
-// $('#newGame').hide();
-// $('#proposition, #try').hide();
-// console.log(randomnumber);
-
-// var clickValue=0;
-// function compteur(){
-//     clickValue = clickValue+1;
-// }
-// $('#play').click(function play() {
-//     $('#proposition, #try').show();
-//     $('#play').hide();
-// });
-
-// $('#try').click(function justPrice() {
-//     compteur();
-//     var proposition = document.getElementById('proposition').value;
-//     if (proposition != '' && proposition >= 0 && proposition <= 100) {
-//         if (proposition > randomnumber) {
-//             $("#player").attr('src', 'assets/wav/cest_moins.wav');
-//             $("#player")[0].play();
-//             $('#result').html('C\'est moins que ' + proposition + ' !');
-//         }
-//         else if (proposition < randomnumber) {
-//             $("#player").attr('src', 'assets/wav/cest_plus.wav');
-//             $("#player")[0].play();
-//             $('#result').html('C\'est plus que ' + proposition + ' !');
-//         }
-//         else if (proposition == randomnumber) {
-//             $("#player").attr('src', 'assets/wav/bien_joue.wav');
-//             $("#player")[0].play();
-//             $('#result').css('color', 'green');
-//             $('#result').html('C\'était effectivement ' + randomnumber + ', bien joué !');
-//             $('#essais').html('Vous avez fait ' + clickValue + ' essai(s)');
-//             $('#proposition, #try').hide();
-//             $('#newGame').show();
-//         }
-//     }
-//     else if (proposition == '') {
-//         $('#result').html('Veuillez entrer une valeur');
-//     }
-//     else {
-//         $("#player").attr('src', 'assets/wav/tabuses.wav');
-//         $("#player")[0].play();
-//         $('#result').html('On a dit entre 0 et 100 !');
-//     }
-// }
-// );
 
 // $('#myModal').modal('toggle');
 // $('#myModal').modal('show');
